ea51d4fa30574fe83835729055d2ecd007c2545c
akassiosc <akassiosc@gmail.com>
2023-11-08 19:09:05 -0300

    inserindo landing page, login, cadastro de usuario etc

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
-onst express = require('express');
+const express = require('express');
 const app = express();
 const port = 3000; //porta padrão
+
+const axios = require('axios');
 
 app.use(require('cors')());
 app.use(express.urlencoded({ extended: true }));
@@ -9,23 +11,30 @@ app.use(express.json());
 //definindo as rotas
 const router = express.Router();
 
+// API de Teste
+
+router.get('/teste', (req, res) => res.json({ message: 'Funcionando!' }));
+
+
 // Configurando o START do servidor
 app.use('/', router);
 
 // API de consulta CEP
 
-app.get(('cep/:cep', async (req, res) => {
-    const { cep } = req.params
+app.get('cep/:cep', async (req, res) => {
+    const { cep } = req.params;
     try {
         const url = `http://viacep.com.br/ws/${cep}/json/`;
+        const response = await axios.get(url);
+        return res.status(200).send(response.data);
 
     } catch (erro) {
+        return res.status(500).send({ error: 'ERRO ao consultar o CEP' });
 
     }
 
-
-}))
+});
 
 //inicia o servidor
 app.listen(port);
-console.log('API funcionando!');
+console.log('Servidor/API funcionando!');
