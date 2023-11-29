5ce6154f119722bc1327cad32a286518fb7f0616
akassiosc <akassiosc@gmail.com>
2023-11-29 00:11:04 -0300

    reverter atualização

--- app.js
+++ app.js
@@ -20,6 +20,14 @@ const UserSchema = new mongoose.Schema({
 
 const User = mongoose.model('User', UserSchema);
 
+app.use((req, res, next) => {
+    // Verificar se o usuário é administrador
+    if (req.body.email !== 'administrador@gmail.com') {
+        return res.status(403).send({ message: 'Acesso negado.' });
+    }
+    next();
+});
+
 // Rota para criar um novo usuário
 app.post('/users', async (req, res) => {
     try {
@@ -61,7 +69,27 @@ app.put('/users/:id', async (req, res) => {
     }
 });
 
-// Rota de processamento de pagamento (exemplo)
+// Rota para login de usuário
+app.post('/login', async (req, res) => {
+    try {
+        const user = await User.findOne({ email: req.body.email });
+        if (!user) {
+            return res.status(401).send({ message: 'Usuário não encontrado.' });
+        }
+
+
+        if (user.password !== req.body.password) {
+            return res.status(401).send({ message: 'Senha incorreta.' });
+        }
+
+        res.send({ message: 'Login bem-sucedido.' });
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
+
+
+// Rota de processamento de pagamento 
 app.post('/processar-pagamento', async (req, res) => {
     console.log('Dados de pagamento:', req.body);
     res.status(200).send({ message: 'Pagamento processado com sucesso!' });
